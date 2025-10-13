07a715dfbf9cbcc0c11010b7a49fac5a782b1669
hewiufewfj <isakd@nemendur.is>
2025-10-13 18:01:36 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
 function openCity(evt, cityName) {
-  var i, tabcontent, tablinks;
-  tabcontent = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
-  }
-  tablinks = document.getElementsByClassName("tablinks");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
-  }
-  document.getElementById(cityName).style.display = "block";
-  evt.currentTarget.className += " active";
+	var i, tabcontent, tablinks;
+	tabcontent = document.getElementsByClassName("tabcontent");
+	for (i = 0; i < tabcontent.length; i++) {
+		tabcontent[i].style.display = "none";
+	}
+	tablinks = document.getElementsByClassName("tablinks");
+	for (i = 0; i < tablinks.length; i++) {
+		tablinks[i].className = tablinks[i].className.replace(" active", "");
+	}
+	document.getElementById(cityName).style.display = "block";
+	evt.currentTarget.className += " active";
 }
+
+
+document.querySelectorAll(".link-list").forEach(list => {
+	for (const li of list.children) {
+		for (const a of li.getElementsByTagName("a")) {
+			if (!a.target) a.target = "_blank"
+		}
+	}
+})
